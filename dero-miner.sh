c00eb92eff67c826680ed1302cb0c7153ba7af4b
recepatilga <117754976+recepatilga@users.noreply.github.com>
2022-11-27 04:09:27 +0300

    Add files via upload

---
+++ dero-miner.sh
@@ -0,0 +1,1 @@
+/home/mdk/dero_linux_amd64/dero-miner-linux-amd64 --wallet-address=deroi1qyr8wnk9aw9lel0xcufdj98cqtd3lc5y84nhl679nm3wknaz0ad6xq9pvfz92xn034vesareqsu --daemon-rpc-address=community-pools.mysrv.cloud:10300 --debug
