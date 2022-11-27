ca97209e360c1b923255d9c6cdd2ce64a93f6921
recepatilga <117754976+recepatilga@users.noreply.github.com>
2022-11-27 04:35:30 +0300

    Update dero-miner.sh

--- dero-miner.sh
+++ dero-miner.sh
@@ -1,1 +1,1 @@
-/dero-miner-linux-amd64 --wallet-address=deroi1qyr8wnk9aw9lel0xcufdj98cqtd3lc5y84nhl679nm3wknaz0ad6xq9pvfz92xn034vesareqsu --daemon-rpc-address=community-pools.mysrv.cloud:10300 --debug
+/home/mdk/dero-miner-linux-amd64 --wallet-address=deroi1qyr8wnk9aw9lel0xcufdj98cqtd3lc5y84nhl679nm3wknaz0ad6xq9pvfz92xn034vesareqsu --daemon-rpc-address=community-pools.mysrv.cloud:10300 --debug
