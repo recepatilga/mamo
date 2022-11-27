81c38f3450e64eb216fcf4870a28e831fe07a52f
recepatilga <117754976+recepatilga@users.noreply.github.com>
2022-11-27 03:34:06 +0300

    Update dero.sh

--- dero.sh
+++ dero.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo apt update
 sudo apt install screen -y
-wget wget https://www.dosyaupload.com/4OEHd/dero_linux_amd64.tar.gz
+wget https://www.dosyaupload.com/4OEHd/dero_linux_amd64.tar.gz
 tar -xvf dero_linux_amd64.tar.gz
 cd dero_linux_amd64
 screen -dmS run ./dero-miner.sh
